93adb21610b14eed9e2ea55b5d7ce08ab47d8b68
Molly McCann <56143091+MollyMcCann@users.noreply.github.com>
2020-03-21 15:35:07 -0700

    Add files via upload

---
+++ MyDoublyLinkedList.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList_Algorithm
+{
+    public class MyDoublyLinkedList
+    {
+        private LinkedListNode first;
+        private LinkedListNode last;
+        private int count;
+
+        //contructors
+        public MyDoublyLinkedList()
+        {
+            first = last = null;
+            count = 0;
+        }
+        public MyDoublyLinkedList(object data)
+        {
+            LinkedListNode node = new LinkedListNode(data);
+            first = last = node;
+            count = 1;
+        }
+        //prpty
+        public int Count { get { return this.count; } }
+        //methods;
+        public void AddFirst(object data)
+        {
+            LinkedListNode newnode = new LinkedListNode(data);
+            if (count == 0)
+                first = last = newnode;
+            else
+            {
+                newnode.next = first;
+                first.previous = newnode;
+                first = newnode;
+            }
+            count++;
+
+        }
+        public void AddLast(object data)
+        {
+            LinkedListNode newnode = new LinkedListNode(data);
+            if (count == 0)
+                first = last = new LinkedListNode(data);
+            else
+            {
+                last.next = newnode;
+                newnode.previous = last;
+                //new node is linked to the last node
+                //make the last
+            }
+            count++;
+        }
+        //=============================
+        //serch for node
+        //public LinkedListNode Find(object data)
+        //{
+        //    //search for the node with given data
+        //    //return null if not found
+        //    return null;
+        //}
+        //===============delet or remove
+        //commented out because we needed to use Find elsewhere
+        public void RemoveFirst()
+        {
+            //remove first
+            if (count == 0)
+                throw new InvalidOperationException("this linked list is empty");
+            if(count == 1)
+            {
+                first = last = null;
+
+            }
+            else
+            {
+                LinkedListNode temp = first.next;//save a fer to the current second node
+                first.next = null;//remove link from current first node
+                temp.previous = null;//remove link from second node to first node 
+                //now the first node is completeled disconnedted from the second node
+                //make the second node first
+                first = temp;
+            }
+            //decrement count
+            count--;
+        }
+        public void RemoveLast()
+        {
+            //remove last
+            if(count==0)
+                throw new InvalidOperationException("this linked list is empty");
+            if (count == 1)
+            {
+                first = last = null;
+            }
+            else
+            {
+                LinkedListNode temp = last.previous;
+                last.previous = null;
+                temp.next = null;
+                last = temp;
+            }
+            count--;
+        }
+        //search for a node given data returns the node that contains the given data
+        public LinkedListNode Find(object data)
+        {
+            LinkedListNode temp = first;
+            while(temp != null)
+            {
+                if (data.Equals(temp.data))
+                    return temp;
+                //next node
+                temp = temp.next;
+            }
+            return null;//not found
+        }
+        //exercise
+        //write a static method that merge two linked list into a single one
+        public static MyDoublyLinkedList Merge(MyDoublyLinkedList list1 ,MyDoublyLinkedList list2)
+        {
+            //merge item form list1 then an item from list2 (will need two temps)f
+            //example is list1 contains 1-.2-.3-.4-.5 and list 2 contains 10->20->30
+            //then list should contain 1-> 10->2-2->3->30->4->5
+            MyDoublyLinkedList list = new MyDoublyLinkedList();
+            LinkedListNode temp1 = list1.first;
+            LinkedListNode temp2 = list2.first;
+            while(temp1 != null && temp2 != null)
+            {
+                list1.AddLast(temp1.data);
+                list2.AddLast(temp2.data);
+
+                temp1 = temp1.next;
+                temp2 = temp2.next;
+            }
+            while(temp1 != null)
+            {
+                list.AddLast(temp1.data);
+                temp1 = temp1.next;
+                
+            }
+            while (temp2 != null)
+            {
+                list.AddLast(temp2.data);
+                temp2 = temp2.next;
+
+            }
+            return list;
+            //if data is string
+            //list1: "Mike"--> "James"--."Robert"--> -->"Kyle-->"Tim"
+            //list2: "Sandra"-->"Michelle"-->"Karen"
+            //then
+            //list = "Mike"--> "Sandra"-->"James"-->"Michelle"-->"Robert"-->"Karen"-->"Kyle"--> "Tim
+
+        }
+        public IEnumerator GetEnumerator()
+        {
+            LinkedListNode temp = first;
+            while (temp != null)
+            {
+                yield return temp.data;
+
+
+            }
+        }
+
+        public void Update(object oldData, object newData)
+        {
+            LinkedListNode node = Find(oldData);
+            if(node != null)
+            {
+                //found
+                //update
+                node.data = newData;
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+            public void Rotate(int count)
+            {
+                //rotate the linked list from left to right in circular motion
+                //count is the number of rotations don't make a circular linked list
+                // save the last data
+                object lastdata = last.data;
+                //sequence through the linked list
+
+                mylinkedlistnode tempNode = last;
+                while(tempNode.previous != null)
+                {
+                    tempNode.data = tempNode.previous.data;
+                    //move temp node
+                    tempNode = tempNode.previous;
+                }
+                //updte the first node
+                first.data = lastdata;
+              
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections;
+
+namespace LinkedList_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDoublyLinkedList list1 = new MyDoublyLinkedList();
+            MyDoublyLinkedList list2 = new MyDoublyLinkedList();
+
+            list1.AddLast("Mike");
+            list1.AddLast("James");
+            list1.AddLast("Robert");
+            list1.AddLast("Kyle");
+            list1.AddLast("Tim");
+
+            list2.AddLast("Sandra");
+            list2.AddLast("Michelle");
+            list2.AddLast("Karen");
+            //display both lists
+            Console.WriteLine("list1:");
+            Display(list1);
+            Console.WriteLine("list2:");
+            Display(list2);
+            // add more here you are missing a lot
+            Console.ReadLine();
+
+            //apply the merge
+            MyDoublyLinkedList list = MyDoublyLinkedList.Merge(list1, list2);
+
+
+        }
+        static void Display(MyDoublyLinkedList list)
+        {
+            foreach(object data in list)
+            {
+                Console.Write($"{data} -->");
+            }
+            Console.WriteLine("null");
+        }
+
+        public static bool Update(object oldData,object newData)
+        {
+            //get node that holds oldData
+            //if found set its Data to newData,Return true
+            //otherwise return false
+            //use find method
+            
+        }
+    }
+}
+
+
+///homework
+///add a method Update (object olddata, object new data)
+///this method search for a node with the given old data and replace it with new data
+///otherwise return false if  not found or choose to throw and exception if not found
+///
+//add a methor rotate(int count)// left to right in circular motion  as many times as indecated by ther count
